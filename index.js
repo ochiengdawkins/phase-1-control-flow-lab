9026dc17589a3a0c401550791789b3387c03cb8b
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-09-03 09:59:04 +0300

    done with Assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let message;
+
+  if (someValue <= 400){
+    message = `This one is on me!`;
+  } else if (someValue > 2000 && someValue <= 2500){
+    message = `I will gladly take your thirty bucks.`;
+  } else if (someValue > 2500){
+    message = `No can do.`;
+  }
+
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message;
+
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+      
+    case "not as generous":
+      message = "Thank you.";
+      break;
+  
+    default:
+      message = "Bye.";
+      break;
+  }
+
+  return message;
 }
